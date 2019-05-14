6b21362623f1843462d20741c535b82a830ebcc6
Florentijn <florentjn@live.nl>
2019-05-14 13:33:02 +0200

    language outside of document head fix

--- search.js
+++ search.js
@@ -51,6 +51,9 @@ function sendSearchApi(value, callback=undefined, id){
     let token = AuthToken
     let numberResponse = 5;
     let lang = document.documentElement.getAttribute("lang")
+	if(lang == "null"){
+		lang == window.location.pathname.split( '/' )[1];
+	}
     req.open('GET', decodeURIComponent('https://api.jibia.nl/api/do_search?query='+value+'&token='+token+'&n='+numberResponse+'&country_code='+lang), true);
     req.addEventListener("readystatechange", function () {
         if (req.readyState === 4) {
