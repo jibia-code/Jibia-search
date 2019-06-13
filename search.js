c986ff63f9e0ebff7f9901547cd8aba90ab9acef
Sodfa <sodfashafik@gmail.com>
2019-06-13 11:42:07 +0200

    Add new layout

--- search.js
+++ search.js
@@ -4,16 +4,30 @@ var AuthToken = document.currentScript.getAttribute('token');
 var ThemeCategory = 2;
 var lang = document.currentScript.getAttribute('lang');
 
+function createTitle(string) {
+  let titleElement = document.createElement("p")
+  titleElement.innerHTML = string;
+  return titleElement;
+}
+
+function createSubtitle(string) {
+  let subtitleElement = document.createElement("i")
+  subtitleElement.innerHTML = string;
+  return subtitleElement;
+}
+
 function makeUL(productsarray,termsarray, categoryarray) {
     var list = document.createElement('ul');
     list.className += "jibia-search-box";
     list.id += "jibia-autocomplete"
-    termsarray.map(function(term){ 
+    let termTitle = createTitle("Zoektermen")
+    list.appendChild(termTitle);
+    termsarray.map(function(term){
         let item = document.createElement('li');
-        item.className += 'jibia-search-element jibia-term-element';    
+        item.className += 'jibia-search-element jibia-term-element';
         item.innerHTML = '<a href = \'' + 'https://' +  window.location.hostname + '/search/' + term['raw_word']  + '\' class = \'jibia-term-link\'><p class = \'jibia-term-title\'>' + term["html_word"] + '</p></a>'
         list.appendChild(item);
-		item.addEventListener("click", function() {
+		  item.addEventListener("click", function() {
 			var req = new XMLHttpRequest();
 			req.open('POST', decodeURIComponent('https://api.jibia.nl/api/search_app_click_analytics'), true);
 			req.setRequestHeader("Content-type", "application/json");
@@ -21,24 +35,30 @@ function makeUL(productsarray,termsarray, categoryarray) {
 			req.send(data);
 		})
 	});
-    productsarray.map(function(name){ 
+
+  let productTitle = createTitle("Artikelen")
+  list.appendChild(productTitle);
+    productsarray.map(function(name){
             let item = document.createElement('li');
-            let prod = name["product"]  
-            item.className += "jibia-search-element jibia-product-element";    
-            item.innerHTML = "<a href = '" + 'https://' +  window.location.hostname + '/' + prod["url"] + '.html' + "' class = 'jibia-product-link'><img class = 'jibia-product-image' src ='" + prod["img_url"] + "'><p class = 'jibia-product-title'>" + prod["name"] + "</p> </a>"//Dit zou dan al veranderd moeten zijn voor Cloudsuite
+            let prod = name["product"]
+            let productCategory = createSubtitle("Babyspullen") //Deze is nog hardcoded
+            item.className += "jibia-search-element jibia-product-element";
+            item.innerHTML = "<a href = '" + 'https://' +  window.location.hostname + '/' + prod["url"] + '.html' + "' class = 'jibia-product-link'><img class = 'jibia-product-image' src ='" + prod["img_url"] + "'>"+ "<div> <i>" +productCategory.innerHTML +"</i> <p class = 'jibia-product-title'>" + prod["name"] + "</p></a></div>"//Dit zou dan al veranderd moeten zijn voor Cloudsuite
             list.appendChild(item);
 		item.addEventListener("click", function() {
-			var req = new XMLHttpRequest();
+			var req = new XMLHttpRequeswet();
 			req.open('POST', decodeURIComponent('https://api.jibia.nl/api/search_app_click_analytics'), true);
 			req.setRequestHeader("Content-type", "application/json");
 			var data = JSON.stringify({"origin":window.location,"query": name});
 			req.send(data);
 		})
-		});
-	categoryarray.map(function(name){	 
+  });
+  let categoryTitle = createTitle("Categoriën")
+  list.appendChild(categoryTitle);
+	categoryarray.map(function(name){
 		let item = document.createElement('li');
-		let cate = name["category"]  
-		item.className += "jibia-search-element jibia-category-element";    
+		let cate = name["category"]
+		item.className += "jibia-search-element jibia-category-element";
 		item.innerHTML = "<a href = '" + 'https://' +  window.location.hostname + '/' + cate["name"] + '.html' + "' class = 'jibia-category-link'><img class = 'jibia-category-image' src ='" + cate["img_url"] + "'><p class = 'jibia-category-title'>" + cate["name"] + "</p> </a>"//Dit zou dan al veranderd moeten zijn voor Cloudsuite
 		list.appendChild(item);
 		item.addEventListener("click", function() {
@@ -48,65 +68,65 @@ function makeUL(productsarray,termsarray, categoryarray) {
 			var data = JSON.stringify({"origin":window.location,"query": name});
 			req.send(data);
 		});
-	});	 
+	});
     return list;
 }
 
-function reloadresults(auto_data){ 
-    var autoCompleteBox = document.getElementById("data");
-	autoCompleteBox.innerHTML = ""; 
+function reloadresults(auto_data){
+  var autoCompleteBox = document.getElementById("data");
+	autoCompleteBox.innerHTML = "";
 	var temp_dict = [{"category" : {name : "citroenen", img_url : "https://upload.wikimedia.org/wikipedia/commons/3/37/Oryctolagus_cuniculus_Tasmania_2.jpg",  }}];
 	var auto_data = {
-		"ref": "https://www.graceisgreen.com/vrouw/", 
+		"ref": "https://www.graceisgreen.com/vrouw/",
 		"result": {
 		  "products": [
 			{
 			  "product": {
-				"img_url": "https://cdn.webshopapp.com/shops/235598/files/228879149/50x50x2/bambooty-basics-wetbag---alle-kleuren.jpg", 
-				"name": "Bambooty Basics Wetbag in verschillende kleuren", 
+				"img_url": "https://cdn.webshopapp.com/shops/235598/files/228879149/50x50x2/bambooty-basics-wetbag---alle-kleuren.jpg",
+				"name": "Bambooty Basics Wetbag in verschillende kleuren",
 				"url": "bambooty-basics-wetbag"
 			  }
-			}, 
+			},
 			{
 			  "product": {
-				"img_url": "https://cdn.webshopapp.com/shops/235598/files/251267771/50x50x2/inlegger-klein-3-2.jpg", 
-				"name": "Bambooty inlegger voor in de wasbare luier - 2 varianten.", 
+				"img_url": "https://cdn.webshopapp.com/shops/235598/files/251267771/50x50x2/inlegger-klein-3-2.jpg",
+				"name": "Bambooty inlegger voor in de wasbare luier - 2 varianten.",
 				"url": "bamboo-inlegger"
 			  }
-			}, 
+			},
 			{
 			  "product": {
-				"img_url": "https://cdn.webshopapp.com/shops/235598/files/211176899/50x50x2/bambooty-zoogcompressen-zakje.jpg", 
-				"name": "Booby wasbare zoogcompressen dag", 
+				"img_url": "https://cdn.webshopapp.com/shops/235598/files/211176899/50x50x2/bambooty-zoogcompressen-zakje.jpg",
+				"name": "Booby wasbare zoogcompressen dag",
 				"url": "booby-wasbare-zoogcompressen-dag"
 			  }
-			}, 
+			},
 			{
 			  "product": {
-				"img_url": "https://cdn.webshopapp.com/shops/235598/files/211177382/50x50x2/zoogcompressen-nacht-wasbaar.jpg", 
-				"name": "Booby wasbare zoogcompressen nacht", 
+				"img_url": "https://cdn.webshopapp.com/shops/235598/files/211177382/50x50x2/zoogcompressen-nacht-wasbaar.jpg",
+				"name": "Booby wasbare zoogcompressen nacht",
 				"url": "booby-wasbare-zoogcompressen-nacht"
 			  }
-			}, 
+			},
 			{
 			  "product": {
-				"img_url": "https://cdn.webshopapp.com/shops/235598/files/263034773/50x50x2/swim-and-training-whale-hq.png", 
-				"name": "Actie! Zwemluier & Trainingsbroekje Bubble - 2-pak  ", 
+				"img_url": "https://cdn.webshopapp.com/shops/235598/files/263034773/50x50x2/swim-and-training-whale-hq.png",
+				"name": "Actie! Zwemluier & Trainingsbroekje Bubble - 2-pak  ",
 				"url": "zwemluier-trainingsbroekje-bubble"
 			  }
 			}
-		  ], 
+		  ],
 		  "words": [
 			{
-			  "html_word": "<b>B</b>orstvoeding", 
+			  "html_word": "<b>B</b>orstvoeding",
 			  "raw_word": "Borstvoeding"
-			}, 
+			},
 			{
-			  "html_word": "<b>B</b>escherming", 
+			  "html_word": "<b>B</b>escherming",
 			  "raw_word": "Bescherming"
-			}, 
+			},
 			{
-			  "html_word": "<b>B</b>abydoekjes", 
+			  "html_word": "<b>B</b>abydoekjes",
 			  "raw_word": "Babydoekjes"
 			}
 		  ]
@@ -139,7 +159,7 @@ function sendSearchApi(value, callback=undefined, id){
         }
     });
     req.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    req.send(); 
+    req.send();
 }
 
 function search(event){
@@ -158,7 +178,7 @@ function popup(event) {
 }
 
 function addevent(){
-	
+
 	let searchbars = document.getElementsByName('q');
 	let searchunit = document.createElement('div');
 	document.onclick = function(e){
@@ -174,7 +194,7 @@ function addevent(){
 		}
 	searchunit.id = "searchunit"
 	searchunit.className = "popup";
-	searchunit.innerHTML = "<div class = 'center-box'/><input id = 'searchbox' class = 'searchbox'></input></div>"; 
+	searchunit.innerHTML = "<div class = 'center-box'/><i class='fa fa-search searchicon'></i><input id = 'searchbox' class = 'searchbox'></input></div>";
 	let i = 0;
 	searchbars.forEach(function(e){
 		searchunit.setAttribute("s", i);
@@ -184,9 +204,9 @@ function addevent(){
 		i = i + 1;
 	});
 	searchunit.addEventListener("input", search);
-	let autoCompleteBox = document.createElement('div'); 
-	autoCompleteBox.id = "data"; 
-	
+	let autoCompleteBox = document.createElement('div');
+	autoCompleteBox.id = "data";
+
 	autoCompleteBox.test = "kut"
-	searchunit.appendChild(autoCompleteBox); 
+	searchunit.appendChild(autoCompleteBox);
 }
