23010c3d2009fae7a4e7f6b53f79d859e1fddd26
Florentijn <florentjn@live.nl>
2019-07-22 15:58:21 +0200

    drop origin from click analytics

--- search.js
+++ search.js
@@ -32,7 +32,7 @@ function add_category(categoryarray, list, lang){
 			var req = new XMLHttpRequest();
 			req.open('POST', decodeURIComponent('https://bapi.jibia.nl/api/search_app_click_analytics'), true);
 			req.setRequestHeader("Content-type", "application/json");
-			var data = JSON.stringify({"origin":window.location,"query": name, "token": AuthToken});
+			var data = JSON.stringify({"query": name, "token": AuthToken});
 			req.send(data);
 		});
 		list.appendChild(item)
@@ -49,7 +49,7 @@ function add_terms(termsarray, list, lang){
 			var req = new XMLHttpRequest();
 			req.open('POST', decodeURIComponent('https://bapi.jibia.nl/api/search_app_click_analytics'), true);
 			req.setRequestHeader("Content-type", "application/json");
-			var data = JSON.stringify({"origin":window.location,"query": term});
+			var data = JSON.stringify({"query": term, "token": AuthToken});
 			req.send(data)
 		})
 		list.appendChild(item)
@@ -68,7 +68,7 @@ function add_products(productsarray, list, lang){
 			var req = new XMLHttpRequeswet();
 			req.open('POST', decodeURIComponent('https://bapi.jibia.nl/api/search_app_click_analytics'), true);
 			req.setRequestHeader("Content-type", "application/json");
-			var data = JSON.stringify({"origin":window.location,"query": name});
+			var data = JSON.stringify({"query": name, "token": AuthToken});
 			req.send(data);
 		})
 		list.appendChild(item)
